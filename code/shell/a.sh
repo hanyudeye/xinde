f8c14ff649678bea1b92e73e4ec4873aae2d6edc
hanyudeye <1191552161@qq.com>
2016-12-22 17:01:57 +0800

    aa

--- code/shell/a.sh
+++ code/shell/a.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-LOG_DIR=/var/log
-ROOT_UID=0
-E_NOTROOT=1
+# LOG_DIR=/var/log
+# ROOT_UID=0
+# E_NOTROOT=1
 
-LINES=50
-if [ "$UID" -ne "$ROOT_UID" ]
-then
-    echo "Must be root to run this script."
-    exit $E_NOTROOT
-fi
+# LINES=50
+# if [ "$UID" -ne "$ROOT_UID" ]
+# then
+#     echo "Must be root to run this script."
+#     exit $E_NOTROOT
+# fi
 
-if [ -n "$1" ]
-then
-    lines=$1
-else
-    lines=$LINES
-fi
+# if [ -n "$1" ]
+# then
+#     lines=$1
+# else
+#     lines=$LINES
+# fi
 
 # E_WRONGARGS=65
 # case "$1" in
@@ -24,8 +24,59 @@ fi
 #     *) lines=$1;;
 # esac
 
+# cd $LOG_DIR
+# echo "Logs cleaned up."
 
-cd $LOG_DIR
-echo "Logs cleaned up."
+# welcome="Hello,world!"
+# chang=5
+# kuan=5
+# let mianji=chang*kuan
+#echo -e "43.3*45 value is\n"
+#echo "43.3*45"|bc		
+#result=$[ chang * kuan ]
+#echo "sqrt(100)"|bc
+#echo "8^3"|bc
+
+
+#cat<<EOF>log.txt
+#this is log file
+#yessgals 
+#EOF
+
+#declare -A ass_array
+#ass_array=([index1]=4 [index2]="nihO")
+#echo ${ass_array[index2]}
+
+# for i in {0..5};
+# do
+#     echo "hello"
+# done
+
+# for((i=0;i<5;i++)){
+
+# echo "hello"
+#        }
+
+# if [ 3 > 2 ];
+# then
+#     echo "right";
+# fi
+
+
+# if [ 3 -eq 3 ];
+# then
+#     echo "equal ";
+# fi
+
+# if [ 3 -eq 3 -o 5 -lt 4 ];
+# then
+#     echo "here";
+# fi
+
+
+#find . \( -name "log*" -o -name "*.sh" \) -print 两者中的一个
+
+
+
 
 exit 0
