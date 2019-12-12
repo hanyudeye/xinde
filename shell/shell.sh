2271c422411f8109fe92ab6e72dd3b04fde8563b
wuming <1191552161@qq.com>
2019-12-12 19:42:10 +0800

    change

--- shell/shell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cat /etc/passwd | awk -F ":" '{if($NR < 15 && $NF~/bash/) {print $0}}';
-awk -F ":" 'BEGIN{sum =0} {sum = sum+$3} END {print sum}'
-cat /etc/passwd | awk -F ":" '{if($3 > 100) {print $0}}'
-
-echo "hello";
-
-echo "nihao";
